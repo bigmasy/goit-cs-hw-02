b3e8af8bbd0e610f86a553f193bf74ed536ad80a
Yuriy <jura.tymosh@gmail.com>
2024-06-19 11:33:04 +0300

    Add files via upload

---
+++ skript/skript.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+filename="URL.txt"
+log="website_status.log"
+>"$log"
+while IFS= read -r line
+do
+    if [[ -n "$line" ]]; then
+        status=$(curl -s -L --head --request GET "$line" | grep "HTTP/2 200")
+        if [[ -n "$status" ]]; then
+            echo "<$line> is UP" >> "$log"
+        else
+            echo "<$line> is Down" >> "$log"
+        fi
+    fi
+done < "$filename"
+echo "Результати записано у файл логів: $log"
