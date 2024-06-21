14675244cc8e49abb5b2e947a8b22cdf7c634820
bigmasy <jura.tymosh@gmail.com>
2024-06-21 12:03:08 +0300

    hw

--- skript/skript.sh
+++ skript/skript.sh
@@ -2,16 +2,18 @@
 
 filename="URL.txt"
 log="website_status.log"
->"$log"
+> "$log"
+
 while IFS= read -r line
 do
     if [[ -n "$line" ]]; then
         status=$(curl -s -L --head --request GET "$line" | grep "HTTP/2 200")
-        if [[ "$status" == "HTTP/2 200" ]]; then
+        if [[ -n "$status" ]]; then
             echo "<$line> is UP" >> "$log"
         else
             echo "<$line> is Down" >> "$log"
-fi
+        fi
     fi
 done < "$filename"
+
 echo "Результати записано у файл логів: $log"
